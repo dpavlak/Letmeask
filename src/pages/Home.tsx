bc7cb926744851acb85c33333904b4695c48816c
david <davidpavlak@hotmail.com>
2021-06-22 18:00:09 -0300

    Adicionando contexto, hooks e estado na autenticação

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,54 +1,50 @@
-import { useHistory } from 'react-router-dom'
+import { useHistory } from "react-router-dom";
+import { useAuth } from "../hooks/useAuth";
 
-import { auth, firebase } from '../services/firebase'
+import illustrationImg from "../assets/images/illustration.svg";
+import logoImg from "../assets/images/logo.svg";
+import googleIconImg from "../assets/images/google-icon.svg";
 
-import illustrationImg from '../assets/images/illustration.svg';
-import logoImg from '../assets/images/logo.svg';
-import googleIconImg from '../assets/images/google-icon.svg';
+import { Button } from "../components/Button";
 
-import { Button } from '../components/Button';
+import "../styles/auth.scss";
 
-import '../styles/auth.scss';
+export function Home() {
+  const history = useHistory();
+  const { user, signInWithGoogle } = useAuth();
 
-export function Home(){
-    const history = useHistory();
-
-    function handleCreateRoom(){
-        const provider = new firebase.auth.GoogleAuthProvider();
-
-        auth.signInWithPopup(provider).then(result => {
-            console.log(result);
-
-            history.push('/rooms/new');
-        })
+  async function handleCreateRoom() {
+    if (!user) {
+      await signInWithGoogle();
     }
 
-    return(
-        <div id="page-auth">
-            <aside>
-                <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
-                <strong>Crie salas de Q&amp;A ao-vivo</strong>
-                <p>Tire as dúvidas da sua audiência em tempo-real</p>
-            </aside>
-            <main>
-                <div className="main-content">
-                    <img src={logoImg} alt="Logo Letmeask" />
-                    <button onClick={handleCreateRoom} className="create-room">
-                        <img src={googleIconImg} alt="Logo do Google" />
-                        Crie sua sala com o Google
-                    </button>
-                    <div className="separator">ou entre em uma sala</div>
-                    <form>
-                        <input
-                        type="text"
-                        placeholder="Digite o código da sala" 
-                        />
-                        <Button type="submit">
-                            Entrar na sala
-                        </Button>
-                    </form>
-                </div>
-            </main>
+    history.push("/rooms/new");
+  }
+
+  return (
+    <div id="page-auth">
+      <aside>
+        <img
+          src={illustrationImg}
+          alt="Ilustração simbolizando perguntas e respostas"
+        />
+        <strong>Crie salas de Q&amp;A ao-vivo</strong>
+        <p>Tire as dúvidas da sua audiência em tempo-real</p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="Logo Letmeask" />
+          <button onClick={handleCreateRoom} className="create-room">
+            <img src={googleIconImg} alt="Logo do Google" />
+            Crie sua sala com o Google
+          </button>
+          <div className="separator">ou entre em uma sala</div>
+          <form>
+            <input type="text" placeholder="Digite o código da sala" />
+            <Button type="submit">Entrar na sala</Button>
+          </form>
         </div>
-    )
+      </main>
+    </div>
+  );
 }
